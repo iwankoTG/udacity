79799eddc1acb24d89261295fa4474967f5b0072
iwankoTG <keiyonezawa0829@gmail.com>
2020-10-18 23:28:29 +0900

    p08 start code

---
+++ p08_CarND-PID-Control-Project/src/PID.cpp
@@ -0,0 +1,50 @@
+#include "PID.h"
+
+/**
+ * TODO: Complete the PID class. You may add any additional desired functions.
+ */
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+  /**
+   * TODO: Initialize PID coefficients (and errors, if needed)
+   */
+   Kp = Kp_;
+   Ki = Ki_;
+   Kd = Kd_;
+
+   p_error = 0.0;
+   i_error = 0.0;
+   d_error = 0.0;
+}
+
+void PID::UpdateError(double cte) {
+  /**
+   * TODO: Update PID errors based on cte.
+   */
+   d_error = cte - p_error;
+   p_error = cte;
+   i_error += cte;
+}
+
+double PID::TotalError() {
+  /**
+   * TODO: Calculate and return the total error
+   */
+  return -Kp*p_error - Ki*i_error - Kd+d_error;
+  //return 0.0;  // TODO: Add your total error calc here!
+}
+
+double PID::CalcSteering() {
+  double steering = -Kp*p_error - Ki*i_error - Kd*d_error;
+
+  if(steering > 1.0){
+    steering = 1.0;
+  }else if (steering < -1.0){
+    steering = -1.0;
+  }
+  return steering;
+}
