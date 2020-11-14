98e99ff5c6cef8db5a9b08ff17c0412f49fad40d
iwankoTG <keiyonezawa0829@gmail.com>
2020-11-14 21:29:21 +0900

    debugged sec08:car moves

--- p11_CarND-Capstone/ros/src/twist_controller/twist_controller.py
+++ p11_CarND-Capstone/ros/src/twist_controller/twist_controller.py
@@ -46,7 +46,7 @@ class Controller(object):
             self.throttle_controller.reset()
             return 0., 0., 0.
 
-        current_vel = sel.vel_lpf.fit(current_vel)
+        current_vel = self.vel_lpf.filt(current_vel)
 
         steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
 
